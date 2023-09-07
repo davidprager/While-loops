d1cfc3332cfbeb72f7399967eb25f3683c7e7c2a
davidprager <david_prager@yahoo.com>
2023-09-07 18:38:54 -0400

    Initial commit

---
+++ Program1.py
@@ -0,0 +1,7 @@
+"""
+CSCI-UA.0002 Spring 2024
+Assignment #4, Program 1
+Your name here
+"""
+
+# This program will generate some patterns, based upon user input and a random character generator
---
+++ Program2.py
@@ -0,0 +1,7 @@
+"""
+CSCI-UA.0002 Spring 2024
+Assignment #4, Program 2
+Your name here
+"""
+
+# This program simulates the basic game of craps
